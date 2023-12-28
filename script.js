bb1c650cf82200e378c8b23e4ea5497950a71d5d
Ramil Aminov <roma_aminov99@list.ru>
2023-12-29 04:25:58 +0600

    restaurant v1.4

---
+++ script.js
@@ -0,0 +1,10 @@
+// let newWindow = window.open('', '','popup')
+//     newWindow.document.write('<h1>Практикуйтесь в написании кода каждый день</h1>')
+// window.open('https://learnjs.ru/', 'LearnJS', 'width=500,height=500,left=650,top=200')
+// let myWindow = window.open('','newPopUp','width=200,height=200,left=400,top=200')
+//     myWindow.document.write('<button id="btn">Кликни на меня</button>')
+    
+//     let getButton = myWindow.document.querySelector('#btn')
+//     getButton.addEventListener('click', () => {
+//         myWindow.opener.document.write('<h1>Текст на исходной страницы заменен</h1>')
+//     })
